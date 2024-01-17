7785b7cb0124bdec654c4a6cfb9f06de770d8090
vinhle <vinhqle.cp@gmail.com>
2024-01-17 22:46:04 +0700

    Update source code for application options and add git ignore file

--- its/main.cpp
+++ its/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 
-#include <CAM.h>
+// Boost libraries
+#include <boost/program_options.hpp>
+
+using namespace std;
+using namespace boost::program_options;
+
+int parse_config(int argc, char **argv) {
+    // Configuration file name
+    string commonConfig;
+
+    options_description desc("Open V2X options");
+    desc.add_options ()
+        ("help,h", "Print all available options")
+        ("common-config", value(&commonConfig), "Path to v2x common config");
+    // Parse command line arguments
+    variables_map vm;
+    store (command_line_parser (argc, argv).options (desc).run (), vm);
+    notify (vm);
+
+    if (vm.count ("help") || !vm.count ("common-config")) {
+        cerr << desc << "\n";
+        return -1;
+    }
+    return 0;
+}
 
 int main(int argc, char **argv) {
-    return 0;
+    int ret = 0;
+    ret = parse_config(argc, argv);    
+
+    return ret;
 }
