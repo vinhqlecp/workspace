f44288548a7351b5c47e2c5a9ee179996d4c8daf
vinhle <vinhqle.cp@gmail.com>
2024-01-17 23:30:43 +0700

    Update const data for simulation topic and add proto files

--- sim/common/constants.hpp
+++ sim/common/constants.hpp
@@ -2,7 +2,7 @@
 
 #include <string>
 
-const std::string LOC_PUB_TOPIC = "sim_location_topic";
-const std::string VEH_PUB_TOPIC = "sim_vehicle_topic";
+const std::string LOC_PUB_TOPIC = "location_topic";
+const std::string VEH_PUB_TOPIC = "vehicle_topic";
 
-const std::string OBJ_SUB_TOPIC = "sim_object_topic";
+const std::string OBJ_SUB_TOPIC = "object_topic";
